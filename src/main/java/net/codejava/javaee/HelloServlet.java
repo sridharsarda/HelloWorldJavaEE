d60006201eaac163dedd6a037522ed3a4093ed66
sridharsarda <60514373+sridharsarda@users.noreply.github.com>
2021-10-28 18:47:57 +0530

    Update HelloServlet.java

--- src/main/java/net/codejava/javaee/HelloServlet.java
+++ src/main/java/net/codejava/javaee/HelloServlet.java
@@ -28,7 +28,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String yourName = request.getParameter("yourName");
-		PrintWriter writer = response.getwriter();
+		Printwriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
 		writer.close();
 	}
