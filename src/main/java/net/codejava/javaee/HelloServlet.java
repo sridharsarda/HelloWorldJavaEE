3f7ce40f36d4e33e2913adac04038b811195ceb8
sridharsarda <60514373+sridharsarda@users.noreply.github.com>
2021-10-28 18:45:32 +0530

    Update HelloServlet.java

--- src/main/java/net/codejava/javaee/HelloServlet.java
+++ src/main/java/net/codejava/javaee/HelloServlet.java
@@ -21,21 +21,14 @@ public class HelloServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-        /** 
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) 
-         */  
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
-                // TODO Auto-generated method stub  
-        response.getWriter().append("Served at: ").append(request.getContextPath());  
-        }  
-  
+          
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String yourName = request.getParameter("yourName");
-		PrintWriter writer = response.getWriter();
+		PrintWriter Writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
 		writer.close();
 	}
